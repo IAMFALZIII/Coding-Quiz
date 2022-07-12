ca58620e53d5b96977b54c42d93fa7698b47865e
IAMFALZIII <flop3z.11@gmail.com>
2022-07-12 12:58:27 -0700

    Added questions to Js

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+var questions = [{
+    title: "Commonly used data types DO NOT include:",
+    answers: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+},
+
+{
+    title: "The condition in an if / else statement is enclosed within ____.",
+    answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+},
+
+{
+    title: "Arrays in Javascript can be used to store ____.",
+    answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
+},
+
+{
+    title: "String values must be enclosed within ____ when being assigned to variables.",
+    answers: ["commas", "curly brackets", "quotes", "parenthesis"],
+    answer: "all of the above"
+},
+
+{
+    title: "String values must be enclosed within ____ when being assigned to variables.",
+    answers: ["commas", "curly brackets", "quotes", "parenthesis"],
+    answer: "quotes"
+},
+
+{
+    title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+    answers: ["Javascript", "terminal / bash", "for loops", "console log"],
+    answer: "console.log"
+}];
